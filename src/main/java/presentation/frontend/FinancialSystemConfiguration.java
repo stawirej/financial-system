facadfafb065e275ff275b408856fcfe83dd1d3b
Piotr Stawirej <piotr.stawirej@gmail.com>
2019-02-20 22:05:30 +0100

    Refactor.

--- src/main/java/presentation/frontend/FinancialSystemConfiguration.java
+++ src/main/java/presentation/frontend/FinancialSystemConfiguration.java
@@ -1,16 +1,16 @@
 package presentation.frontend;
 
 import application.FinancialSystem;
-import application.FinancialSystemConfiguration;
 import infrastructure.ProductionRepository;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@org.springframework.context.annotation.Configuration
-class Configuration {
+@Configuration
+class FinancialSystemConfiguration {
 
     @Bean
     public FinancialSystem financialSystem() {
-        return FinancialSystemConfiguration.financialSystem(ProductionRepository.newInstance());
+        return application.FinancialSystemConfiguration.financialSystem(ProductionRepository.newInstance());
     }
 
 }
